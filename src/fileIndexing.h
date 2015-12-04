5b816d670eea1526d1e4232423b1bcd9549192b8
Franck-Junior <franckjunior.clerget@gmail.com>
2015-12-04 20:18:00 +0100

    add config file, start text indexing header

--- src/fileIndexing.h
+++
@@ -1,5 +0,0 @@
-/**
-* \author Franck CLERGET
-* \date 4 december 2015
-* \struct fileIndexing fileIndexing.h "The struct and function about file indexing"
-*/
---
+++ src/textIndexing.h
@@ -0,0 +1,29 @@
+/**
+* \author Franck CLERGET
+* \date 4 december 2015
+* \file textIndexing.h
+* \brief The struct and function about text indexing.
+* Method to index : ls <fileName> | XXX.sh >> baseTextDescriptor.txt
+* Where XXX.sh is the text indexing script.
+*/
+
+/**
+* \struct therm
+* \brief
+* therm contains a word with his occurence.
+*/
+struct therm {
+    char[20] word;
+    unsigned int occurence;
+}
+
+/**
+* \struct textDescriptor
+* \brief
+* The text descriptor contains information about a specific text file.
+*/
+struct textDescriptor {
+    therm[] thermList; // pourquoi pas utiliser une pile/file
+};
+
+
