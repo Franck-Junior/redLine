7f04bdebf0ab509b923889966e4b179107950997
Tonze <aurelien.vincotte@gmail.com>
2016-01-08 15:51:31 +0100

    Config+interface

---
+++ src/config.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+char * getConfig(char * request) {
+	FILE *fp;
+	if ((fp = fopen("config.txt", "r+")) == NULL);
+	char temp[92];
+	char * res;
+	char * line;
+	while(fgets(temp,92,fp) != NULL) {
+		if ((strstr(temp, request)) != NULL) {
+			line = strdup(temp);
+		}
+	}
+
+	if(fp) {
+		fclose(fp);
+	}
+
+	res = strtok(line,"=");
+	res = strtok(NULL,"=");
+	return res;
+}
